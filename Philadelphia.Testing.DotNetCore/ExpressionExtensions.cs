8c4c7d86ad07e6ee1fa44c6c3133cf3aaae5b77e
Tomasz Sztokinier <tomasz@todoit.pl>
2019-04-25 15:57:23 +0200

    testing: expression evaluation improvements

---
+++ Philadelphia.Testing.DotNetCore/ExpressionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Philadelphia.Testing.DotNetCore {
+    public static class ExpressionExtensions {
+        public static string Describe(this Expression expr) => $"{expr?.GetType().Name}({expr}:{expr?.Type})";
+        public static object ValueOf(this Expression expr) {
+            switch (expr) {
+                case ConstantExpression x: return x.Value;
+                case MemberExpression x:
+                    switch (x.Member) {
+                        case FieldInfo m: return m.GetValue(ValueOf(x.Expression));
+                        case PropertyInfo m: return m.GetValue(ValueOf(x.Expression));
+                        default:
+                            throw new Exception($"Member expresion not supported for member type {x.Member.GetType().Name}\n{Describe(expr)}");
+                    }
+                case null: return null;
+                default: throw new Exception("Expresion not supported: " + Describe(expr));
+            }
+        }
+    }
+}
--- Philadelphia.Testing.DotNetCore/HttpServiceUtil.cs
+++ Philadelphia.Testing.DotNetCore/HttpServiceUtil.cs
@@ -7,6 +7,7 @@ using Philadelphia.Common;
 
 namespace Philadelphia.Testing.DotNetCore {
     public static class HttpServiceUtil {
+
         public static T OfMethodCallExpression<T>(
             MethodCallExpression body, 
             Func<MethodInfo,Type,List<object>,T> bld) {
@@ -14,38 +15,7 @@ namespace Philadelphia.Testing.DotNetCore {
             var m = body.Method;
             var i = m.DeclaringType;
 
-            var prms = 
-                body.Arguments
-                    .Select(x => {
-                        //constant (unlikely)
-                        if (x is ConstantExpression) {
-                            return ((ConstantExpression)x).Value;
-                        }
-
-                        //or simple variable
-                        if (!(x is MemberExpression)) {
-                            throw new Exception("could not treat parameter as MemberExpression");
-                        }
-
-                        var mx = (MemberExpression)x;
-                        if (!(mx.Member is FieldInfo)) {
-                            throw new Exception("parameter's field Member is not FieldInfo");
-                        }
-
-                        if (mx.Expression == null) {
-                            var fi = (FieldInfo)mx.Member;
-                            return fi.GetValue(mx);
-                        }
-
-                        if (!(mx.Expression is ConstantExpression)) {
-                            throw new Exception("parameter's field Expression is not ConstantExpression");
-                        }
-
-                        var cmxe = (ConstantExpression)mx.Expression;
-                        var fmx = (FieldInfo)mx.Member;
-                        return fmx.GetValue(cmxe.Value);
-                    })
-                    .ToList();
+            var prms = body.Arguments.Select(ExpressionExtensions.ValueOf).ToList();
 
             if (!i.IsInterface) {
                 throw new Exception("expected method's declaring type to be service interface");
