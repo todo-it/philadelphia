3d2a8c877d3edbd9d7f6ddab01bd92367a00f88d
Dominik Pytlewski <dominik@pytlewski.eu>
2019-06-25 14:49:36 +0200

    adapted demo code due to "datetime pickers: ability to have custom datetime builder"

--- Philadelphia.Demo.Client/DateTimePicker/DateTimeDemoFormView.cs
+++ Philadelphia.Demo.Client/DateTimePicker/DateTimeDemoFormView.cs
@@ -11,89 +11,95 @@ namespace Philadelphia.Demo.Client {
         public DateTimeRangeView RangeY {get; } = new DateTimeRangeView("Y range", "From", "To", 
             DateTimeFormat.Y, 
             I18n.GetDateTimeFormatForPrecision(DateTimeFormat.Y),
-            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)), 
-            y => y.DateOnly().WithTime(0, 0, 0),
-            y => y.DateOnly().WithTime(23, 59, 59),
+            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)),
             Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
-            PopupLocation.Right);
+            PopupLocation.Right,
+            LocalDateTimeBuilder.InstanceBeginOfDay,
+            LocalDateTimeBuilder.InstanceEndOfDay);
 
         public DateTimeRangeView RangeYM {get; } = new DateTimeRangeView("YM range", "From", "To", 
             DateTimeFormat.YM, 
             I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YM),
-            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)), 
-            y => y.DateOnly().WithTime(0, 0, 0),
-            y => y.DateOnly().WithTime(23, 59, 59),
+            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)),
             Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
-            PopupLocation.Right);
+            PopupLocation.Right,
+            LocalDateTimeBuilder.InstanceBeginOfDay,
+            LocalDateTimeBuilder.InstanceEndOfDay);
 
         public DateTimeRangeView RangeYMD {get; } = new DateTimeRangeView("YMD range", "From", "To", 
             DateTimeFormat.DateOnly, 
             I18n.GetDateTimeFormatForPrecision(DateTimeFormat.DateOnly),
-            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)), 
-            y => y.DateOnly().WithTime(0, 0, 0),
-            y => y.DateOnly().WithTime(23, 59, 59),
+            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)),
             Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
-            PopupLocation.Right);
+            PopupLocation.Right,
+            LocalDateTimeBuilder.InstanceBeginOfDay,
+            LocalDateTimeBuilder.InstanceEndOfDay );
 
         public DateTimeRangeView RangeYMDhm {get; } = new DateTimeRangeView("YMDhm range", "From", "To", 
             DateTimeFormat.YMDhm, 
             I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YMDhm),
-            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)), 
-            y => y.DateOnly().WithTime(0, 0, 0),
-            y => y.DateOnly().WithTime(23, 59, 59),
+            new Tuple<DateTime?,DateTime?>(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-1)),
             Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
-            PopupLocation.Right);
+            PopupLocation.Right,
+            LocalDateTimeBuilder.InstanceBeginOfDay,
+            LocalDateTimeBuilder.InstanceEndOfDay);
 
         public DateTimePickerView SingleYEntry { get; } = DateTimePickerView.CreateSoleEntry(
-                "Y single", 
-                DateTimeFormat.Y, 
-                I18n.GetDateTimeFormatForPrecision(DateTimeFormat.Y),
-                DateTime.Now, 
-                y => y.DateOnly().Equals(DateTime.Now.DateOnly()) ? y.DateOnly().WithTime(23, 59, 59) : y.DateOnly().WithTime(12, 0, 0), 
-                Tuple.Create<DateTime?,DateTime?>(null,DateTimeExtensions.BuildSoonestMidnight()) );
+            "Y single", 
+            DateTimeFormat.Y, 
+            I18n.GetDateTimeFormatForPrecision(DateTimeFormat.Y),
+            DateTime.Now, 
+            Tuple.Create<DateTime?,DateTime?>(null,DateTimeExtensions.BuildSoonestMidnight()),
+            LocalDateTimeBuilder.InstanceBeginOfDay);
 
         public DateTimePickerView SingleYMEntry { get; } = DateTimePickerView.CreateSoleEntry(
-                "YM single", 
-                DateTimeFormat.YM, 
-                I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YM),
-                DateTime.Now, 
-                y => y.DateOnly().Equals(DateTime.Now.DateOnly()) ? y.DateOnly().WithTime(23, 59, 59) : y.DateOnly().WithTime(12, 0, 0), 
-                Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()) );
+            "YM single", 
+            DateTimeFormat.YM, 
+            I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YM),
+            DateTime.Now, 
+            Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
+            LocalDateTimeBuilder.InstanceBeginOfDay);
 
         public DateTimePickerView SingleYMDEntry { get; } = DateTimePickerView.CreateSoleEntry(
-                "YMD single", 
-                DateTimeFormat.DateOnly, 
-                I18n.GetDateTimeFormatForPrecision(DateTimeFormat.DateOnly),
-                DateTime.Now, 
-                y => y.DateOnly().Equals(DateTime.Now.DateOnly()) ? y.DateOnly().WithTime(23, 59, 59) : y.DateOnly().WithTime(12, 0, 0), 
-                Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()) );
+            "YMD single", 
+            DateTimeFormat.DateOnly, 
+            I18n.GetDateTimeFormatForPrecision(DateTimeFormat.DateOnly),
+            DateTime.Now, 
+            Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
+            LocalDateTimeBuilder.Custom(
+                y => DateTimeExtensions.IsToday(y) 
+                ? y.DateOnly().WithTime(23, 59, 59) 
+                : y.DateOnly().WithTime(12, 0, 0) ) );
 
         public DateTimePickerView SingleYMDhmEntry { get; } = DateTimePickerView.CreateSoleEntry(
-                "YMDhm single", 
-                DateTimeFormat.YMDhm, 
-                I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YMDhm),
-                DateTime.Now, 
-                y => y.DateOnly().Equals(DateTime.Now.DateOnly()) ? y.DateOnly().WithTime(23, 59, 59) : y.DateOnly().WithTime(12, 0, 0), 
-                Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()) );
+            "YMDhm single", 
+            DateTimeFormat.YMDhm, 
+            I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YMDhm),
+            DateTime.Now, 
+            Tuple.Create<DateTime?,DateTime?>(null, DateTimeExtensions.BuildSoonestMidnight()),
+            LocalDateTimeBuilder.Custom(
+                y => DateTimeExtensions.IsToday(y) 
+                    ? y.DateOnly().WithTime(23, 59, 59) 
+                    : y.DateOnly().WithTime(12, 0, 0) ));
 
         public DateTimePickerView SingleValidation { get; } = DateTimePickerView.CreateSoleEntry(
                 "Datetime with even day&hour", 
                 DateTimeFormat.YMDhm, 
                 I18n.GetDateTimeFormatForPrecision(DateTimeFormat.YMDhm),
-                null, 
-                y => y.DateOnly().WithTime(0, 0, 0), 
-                Tuple.Create<DateTime?,DateTime?>(null, null) )
+                null,
+                Tuple.Create<DateTime?,DateTime?>(null, null),
+                LocalDateTimeBuilder.InstanceBeginOfDay)
             .With(x => x.Widget.Style.Display = Display.Flex);
 
         public DateTimeRangeView RangeValidation { get; } = 
             new DateTimeRangeView("Three weekdays", "From", "To", 
                 DateTimeFormat.DateOnly, 
                 I18n.GetDateTimeFormatForPrecision(DateTimeFormat.DateOnly),
-                new Tuple<DateTime?,DateTime?>(null, null), 
-                y => y.DateOnly().WithTime(0, 0, 0),
-                y => y.DateOnly().WithTime(0, 0, 0),
+                new Tuple<DateTime?,DateTime?>(null, null),
                 Tuple.Create<DateTime?,DateTime?>(null, null),
-                PopupLocation.Right);
+                PopupLocation.Right,
+                LocalDateTimeBuilder.InstanceBeginOfDay,
+                LocalDateTimeBuilder.InstanceBeginOfDay);
         
         private const string HelpHtml = @"Dates after tomorrow are not available in order to demonstrate such feature in calendar<br>
 Tab and shift-tab is supported with popups auto hiding&showing on focus change<br> 
@@ -119,6 +125,7 @@ Date time formats are fully customizable(think: builder.Add(datetimepart field) 
                                 RangeY.Widget, new HTMLBRElement(), RangeYM.Widget, new HTMLBRElement(),
                                 RangeYMD.Widget, new HTMLBRElement(), RangeYMDhm.Widget );}))),
                     Tuple.Create<string,Action<HTMLElement>>("Validation", cntnr => cntnr.AppendAllChildren(
+                        new HTMLSpanElement {InnerHTML = "Custom input validation in action" }.With(x => x.ClassList.Add("grayedOut")),
                         SingleValidation.Widget.With(x => {
                             x.Style.Display = Display.Flex;
                             x.Style.AlignItems = AlignItems.Center; } ), 
