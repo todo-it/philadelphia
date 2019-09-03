56faff2efd36417d6c1366d4b140e2e675cf3e2d
Tomasz Sztokinier <tomasz@todoit.pl>
2019-09-03 09:50:48 +0200

    common: add some Nullable extensions

--- Philadelphia.Server.Common/StdLibExtensions.fs
+++ Philadelphia.Server.Common/StdLibExtensions.fs
@@ -1,9 +1,18 @@
 ﻿[<AutoOpen>]
 module Philadelphia.Server.Common.StdLibExtensions
 open System.Collections.Generic
+open System
 
 type IDictionary<'Key, 'Value> with
     member inline d.MaybeGetValue key =
         match d.TryGetValue key with
         | false, _ -> None
         | true, v -> Some v
+
+module Nullable =
+    let map fn (x:Nullable<_>) =
+        if x.HasValue 
+        then fn x.Value |> Nullable<_>
+        else x
+
+    let(|Some|None|) (x:Nullable<_>) = if x.HasValue then Some x.Value else None
