4e20a79d51e52283a318845f5c7d2a08f5f94c11
Tomasz Sztokinier <tomasz@todoit.pl>
2019-09-06 09:23:17 +0200

    common util

---
+++ Philadelphia.Common/Utils/Disposable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Philadelphia.Common
+{
+    public class Disposable : IDisposable
+    {
+        private readonly Action _onDispose;
+
+        public Disposable(Action onDispose)
+        {
+            _onDispose = onDispose;
+        }
+
+        public void Dispose()
+        {
+            _onDispose?.Invoke();
+        }
+
+        public static void DisposeBestEffort(IEnumerable<IDisposable> disposables)
+        {
+            Exception TryOne(IDisposable d)
+            {
+                try
+                {
+                    d.Dispose();
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    return e;
+                }
+            }
+
+            var errors = disposables.Select(TryOne).Where(x => x != null).ToList();
+            if (errors.Count > 0)
+            {
+                throw new AggregateException(errors);
+            }
+        }
+    }
+}
