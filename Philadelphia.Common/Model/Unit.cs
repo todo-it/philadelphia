40998e68cb426922a234316213a15a0eef1f1425
Dominik Pytlewski <dominik@pytlewski.eu>
2019-12-09 15:18:53 +0100

    Unit gets private constructor due to JSON deserialization

--- Philadelphia.Common/Model/Unit.cs
+++ Philadelphia.Common/Model/Unit.cs
@@ -3,7 +3,18 @@
     /// F# Unit clone / something without no value or irrelevant value
     /// </summary>
     public class Unit {
-		private Unit() {}
+        /// <summary>
+        /// don't use directly. It is public to make JSON deserialization working
+        /// </summary>
+        public Unit() {}
 		public static readonly Unit Instance = new Unit();
-	}
+
+        public override bool Equals(object obj) {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode() {
+            return 0;
+        }
+    }
 }
