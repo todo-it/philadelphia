a15bbd40e703e7ca0137b51b76d78973a003c804
Dominik Pytlewski <dominik@pytlewski.eu>
2020-11-27 10:46:18 +0100

    updated code to use nonobsoleted methods

--- Philadelphia.Demo.Client/FormsNavigation/SomeChoicesFormView.cs
+++ Philadelphia.Demo.Client/FormsNavigation/SomeChoicesFormView.cs
@@ -1,16 +1,23 @@
-﻿using Bridge.Html5;
+﻿using System;
+using Bridge.Html5;
 using Philadelphia.Common;
 using Philadelphia.Web;
 
 namespace Philadelphia.Demo.Client {
     public class SomeChoicesFormView : IFormView<HTMLElement> {
         public IActionView<HTMLElement> First {get;} 
-            = InputTypeButtonActionView.CreateFontAwesomeIconedButton(
-                IconFontType.FontAwesomeSolid, "First", FontAwesomeSolid.IconChevronLeft);
+            = new InputTypeButtonActionView(new LabelDescr {
+                PreLabelIcon = Tuple.Create(IconFontType.FontAwesomeSolid, FontAwesomeSolid.IconChevronLeft),
+                Label = "First"
+            });
         public IActionView<HTMLElement> Second {get;}
-            = InputTypeButtonActionView.CreateFontAwesomeIconedButton(
-                IconFontType.FontAwesomeSolid, "Second", FontAwesomeSolid.IconChevronRight)
-                .With(x => x.Widget.SetValuelessAttribute(Magics.AttrAlignToRight));
+            = new InputTypeButtonActionView(
+                new LabelDescr {
+                    PreLabelIcon = Tuple.Create(IconFontType.FontAwesomeSolid, FontAwesomeSolid.IconChevronRight),
+                    Label = "Second" 
+                }, 
+                LeftOrRight.Right)
+                    .With(x => x.Widget.SetValuelessAttribute(Magics.AttrAlignToRight));
         public IView<HTMLElement>[] Actions => new IView<HTMLElement>[]{First, Second};
 
         public RenderElem<HTMLElement>[] Render(HTMLElement _) {
