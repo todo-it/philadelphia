04d658502e0fc5555a90cedee56fe2b39d967f8c
Dominik Pytlewski <dominik@pytlewski.eu>
2020-12-17 15:04:03 +0100

    uploadview related validator

---
+++ Philadelphia.Web/Mvp/ValidatorWeb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Philadelphia.Common;
+
+namespace Philadelphia.Web {
+    public static class ValidatorWeb {
+        public static Validate<List<RemoteFileDescr>> LimitSize(int maxSize) =>
+            (newVal, errors) => errors.IfTrueAdd(
+                newVal != null && newVal.Count > maxSize,
+                I18n.Translate("Not allowed to have more than {0} files")
+                    .MessageFormat(maxSize));
+    }
+}
