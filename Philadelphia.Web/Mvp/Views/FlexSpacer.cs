8e6ec3fd2c4cd1a9104c73e4fdb4c47a92c4b0c9
Dominik Pytlewski <dominik@pytlewski.eu>
2020-12-17 15:15:01 +0100

    flexspacer

---
+++ Philadelphia.Web/Mvp/Views/FlexSpacer.cs
@@ -0,0 +1,9 @@
+﻿using Bridge.Html5;
+using Philadelphia.Common;
+
+namespace Philadelphia.Web {
+    public class FlexSpacer : IView<HTMLElement> {
+        private readonly HTMLElement _el = (new HTMLDivElement()).With(x => x.Style.Flex = "1");
+        public HTMLElement Widget => _el;
+    }
+}
